fe6c6dc243c651f2e5953bf90fce849c6f3743df
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-09-25 20:10:54 +0000

    Pushed from Dropbox

--- login_submit.php
+++ login_submit.php
@@ -43,97 +43,95 @@ if ($_SERVER["REQUEST_METHOD"]=="POST"){
 	//--------------------------------
 	// □ submitが押されたら
 	//--------------------------------
-		echo("えらい");
-		//--------------------------------
-		// □ POSTされたデータを取得
-		//--------------------------------
-		$usr_id = htmlspecialchars($_POST["usr_id"], ENT_QUOTES);	//ID
-		$usr_pw = htmlspecialchars($_POST["usr_pw"], ENT_QUOTES);	//パスワード
-		//--------------------------------
-		// □ 入力内容チェック
-		//--------------------------------
-		//ユーザID
-		if (strlen($usr_id)==0){$error = "ユーザIDが入力されていません";}
-		//パスワード
-		if (strlen($usr_pw)==0){$error = "パスワードが入力されていません";}
-		//エラーなし
-		if (strlen($error)==NULL){
-			echo("えらーないよ！");
-			//--------------------------------------------
-			// □ 会員情報テーブル(friendinfo)をチェック
-			//--------------------------------------------
-			$pgsql->query("SELECT * FROM friendinfo WHERE id='$usr_id'");
-			$row = $pgsql->fetch();
-			echo($row);
-			if ($row[]){//IDが存在した場合
-				echo("てすと1");
-				if ($row["pw"] == $usr_pw){
-					//$_SESSION["my_no"] = $row["no"];
-					//$_SESSION["my_id"] = $usr_id;
-					//$_SESSION["my_login"] = 1;
-					//------------------------------------
-					// □ クッキーを保存する
-					//------------------------------------
-					//setcookie("usr_id",$usr_id);//ユーザIDを保存
-					//------------------------------------
-					// □ トップページへジャンプ
-					//------------------------------------
-					echo("てすとおｋ");
-					header("Location: ./top.php"); //トップページへ(ゆくゆくはindex.php)
-					exit;
-				}else{
-					$login_url = './login_fb.php';
-					$login_html = '<a href="'.$login_url.'">ログインページ</a>';
-					echo "Passwords が間違っています。";
-					echo $login_html."よりログインし直して下さい。";
-				}
-			}else{	//IDが存在しない場合
+	echo("えらい");
+	//--------------------------------
+	// □ POSTされたデータを取得
+	//--------------------------------
+	$usr_id = htmlspecialchars($_POST["usr_id"], ENT_QUOTES);	//ID
+	$usr_pw = htmlspecialchars($_POST["usr_pw"], ENT_QUOTES);	//パスワード
+	//--------------------------------
+	// □ 入力内容チェック
+	//--------------------------------
+	//ユーザID
+	if (strlen($usr_id)==0){$error = "ユーザIDが入力されていません";}
+	//パスワード
+	if (strlen($usr_pw)==0){$error = "パスワードが入力されていません";}
+	//エラーなし
+	if (strlen($error)==NULL){
+		echo("えらーないよ！");
+		//--------------------------------------------
+		// □ 会員情報テーブル(friendinfo)をチェック
+		//--------------------------------------------
+		$pgsql->query("SELECT * FROM friendinfo WHERE id='$usr_id'");
+		$row = $pgsql->fetch();
+		if (isset($row['id'])){//IDが存在した場合
+			echo("てすと1");
+			if ($row["pw"] == $usr_pw){
+				//$_SESSION["my_no"] = $row["no"];
+				//$_SESSION["my_id"] = $usr_id;
+				//$_SESSION["my_login"] = 1;
+				//------------------------------------
+				// □ クッキーを保存する
+				//------------------------------------
+				//setcookie("usr_id",$usr_id);//ユーザIDを保存
+				//------------------------------------
+				// □ トップページへジャンプ
+				//------------------------------------
+				echo("てすとおｋ");
+				header("Location: ./top.php"); //トップページへ(ゆくゆくはindex.php)
+				exit;
+			}else{
 				$login_url = './login_fb.php';
 				$login_html = '<a href="'.$login_url.'">ログインページ</a>';
-				$signup_url = './sregister2.php';
-				$signup_html = '<a href="'.$signup_url.'">会員登録ページ</a>';
-
-				echo "IDが間違っています。";
+				echo "Passwords が間違っています。";
 				echo $login_html."よりログインし直して下さい。";
-				echo "登録がまだの方は".$signup_html."より登録してください。";
-				/*
-				//------------------------------------
-				// □ 友達テーブル(friends)をチェック
-				//------------------------------------
-				$sql = "SELECT friends.no,friendinfo.usrid FROM friends";
-				$sql.= " LEFT JOIN friendinfo ON friends.no=friendinfo.no";
-				$sql.= " WHERE friends.email='$usr_id'";
-				$pgsql->query($sql);
-				print $sql;
-				if ($pgsql->rows()>0){
-					$row = $pgsql->fetch();
-					//----------------------------------------------
-					// 一番最初にログインするときのチェック
-					//----------------------------------------------
-					if (!$row["usrid"] && $usr_pw == FIRST_PASS){
-						print "1番最初にログインですね？";
-						$_SESSION["my_no"] = $row["no"];
-						$_SESSION["my_login"] = 1;
-						//------------------------------------
-						// □ マイ情報設定ページへジャンプ
-						//------------------------------------
-						header("Location: http://$host/friendinfo.php");
-						exit;
-					}
-				}else{
-					print "ミス！";
-				}
-				*/
 			}
-			//$error = "ユーザIDかパスワードに誤りがあります";
+		}else{	//IDが存在しない場合
+			$login_url = './login_fb.php';
+			$login_html = '<a href="'.$login_url.'">ログインページ</a>';
+			$signup_url = './sregister2.php';
+			$signup_html = '<a href="'.$signup_url.'">会員登録ページ</a>';
+
+			echo "IDが間違っています。";
+			echo $login_html."よりログインし直して下さい。";
+			echo "登録がまだの方は".$signup_html."より登録してください。";
+			/*
+			//------------------------------------
+			// □ 友達テーブル(friends)をチェック
+			//------------------------------------
+			$sql = "SELECT friends.no,friendinfo.usrid FROM friends";
+			$sql.= " LEFT JOIN friendinfo ON friends.no=friendinfo.no";
+			$sql.= " WHERE friends.email='$usr_id'";
+			$pgsql->query($sql);
+			print $sql;
+			if ($pgsql->rows()>0){
+				$row = $pgsql->fetch();
+				//----------------------------------------------
+				// 一番最初にログインするときのチェック
+				//----------------------------------------------
+				if (!$row["usrid"] && $usr_pw == FIRST_PASS){
+					print "1番最初にログインですね？";
+					$_SESSION["my_no"] = $row["no"];
+					$_SESSION["my_login"] = 1;
+					//------------------------------------
+					// □ マイ情報設定ページへジャンプ
+					//------------------------------------
+					header("Location: http://$host/friendinfo.php");
+					exit;
+				}
+			}else{
+				print "ミス！";
+			}
+			*/
 		}
-	
+		//$error = "ユーザIDかパスワードに誤りがあります";
+	}
 }
 //--------------------------------------------------------------------
 // ■ ブラウザからHTMLページを要求されたとき(クッキーを取得する)
 //--------------------------------------------------------------------
-if ($_SERVER["REQUEST_METHOD"]!="POST"){
-	if (isset($_COOKIE["sns"])) {
+	if ($_SERVER["REQUEST_METHOD"]!="POST"){
+		if (isset($_COOKIE["sns"])) {
 		$sns = $_COOKIE["sns"];	//クッキーを変数に保存
 		$usr_id = $sns["usrid"];	//ユーザIDを取得
 		$usr_pw= $sns["usrpw"];	//ユーザパスワードを取得
