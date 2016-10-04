6353e46b1cf9a0d67899be6d692c0903bfb4c99e
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-10-04 22:06:28 +0000

    Pushed from Dropbox

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ require_once __DIR__ . '/facebook-sdk-v5/autoload.php';
 $fb = new Facebook\Facebook([
   'app_id' => '783967058409220', // Replace {app-id} with your app id
   'app_secret' => 'SECRET',
-  'default_graph_version' => 'v2.2',
+  'default_graph_version' => 'v2.7',
   ]);
 
 $helper = $fb->getRedirectLoginHelper();
