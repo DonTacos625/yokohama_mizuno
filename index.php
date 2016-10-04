225ef59d229979e0899db9d39130531c84f1c7aa
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-10-04 09:15:04 +0000

    Pushed from Dropbox

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+  ini_set( 'display_errors', 1 );
   require_once("./Facebook/Facebook.php");
   $fb = new Facebook([
    'app_id' => 'ID', // Replace {app-id} with your app id
@@ -12,5 +13,4 @@
  $loginUrl = $helper->getLoginUrl('https://websitetest1234.herokuapp.com/fb-callback.php', $permissions);
 
  echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>'; 
- ini_set( 'display_errors', 1 );
 ?>
