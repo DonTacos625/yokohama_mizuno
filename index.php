5735e812a26ae77afee50279c2a794653bc9979d
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-10-04 07:44:00 +0000

    Pushed from Dropbox

--- index.php
+++ index.php
@@ -1,36 +1,15 @@
 <?php
   //Facebook SDK for PHP の src/ にあるファイルを
   //サーバ内の適当な場所にコピーしておく
-  require_once('./src/facebook.php');
   $config = array(
     'appId'  => 'ID',
     'secret' => 'SECRET'
   );
+  require_once('./src/facebook.php');
   $facebook = new Facebook($config);
-  //ログイン済みの場合はユーザー情報を取得
-  if ($facebook->getUser()) {
-    try {
-      $user = $facebook->api('/me','GET');
-    } catch(FacebookApiException $e) {
-      //取得に失敗したら例外をキャッチしてエラーログに出力
-      error_log($e->getType());
-      error_log($e->getMessage());
-    }
-  }
+
+  $feed = $facebook->api('/me/id', 'GET');
+
+  // 結果を出力
+  var_dump($feed);
 ?>
-<html>
-  <body>
-  <?php
-    if (isset($user)) {
-      //ログイン済みでユーザー情報が取れていれば表示
-      echo '<pre>';
-      print_r($user);
-      echo '</pre>';
-    } else {
-      //未ログインならログイン URL を取得してリンクを出力
-      $loginUrl = $facebook->getLoginUrl();
-      echo '<a href="' . $loginUrl . '">Login with Facebook</a>';
-    }
-  ?>
-  </body>
-</html>
