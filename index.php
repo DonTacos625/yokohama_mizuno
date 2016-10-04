2103af096684f7044d5ebdb19869f47ab326c032
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-10-04 08:26:16 +0000

    Pushed from Dropbox

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
   ini_set( 'display_errors', 1 );
-  require_once("./Facebook/Facebook.php")
+  require_once("./Facebook/Facebook.php");
   $fb = new Facebook([
    'app_id' => 'ID', // Replace {app-id} with your app id
    'app_secret' => 'SECRET',
