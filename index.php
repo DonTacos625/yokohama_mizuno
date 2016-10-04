439323729e23955f8e4f1efcab27bac327024eaf
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-10-04 09:33:27 +0000

    Pushed from Dropbox

--- index.php
+++ index.php
@@ -1,15 +1,31 @@
 <?php
   ini_set( 'display_errors', 1 );
-  $fb = new Facebook\Facebook([
-   'app_id' => 'ID', // Replace {app-id} with your app id
-   'app_secret' => 'SECRET',
-   'default_graph_version' => 'v2.2',
-   ]);
+session_start();
 
- $helper = $fb->getRedirectLoginHelper();
+// autoload.phpの場所(パス)を記載
+require_once __DIR__ . '/Facebook/autoload.php';
 
- $permissions = ['email'];  // Optional permissions
- $loginUrl = $helper->getLoginUrl('https://websitetest1234.herokuapp.com/fb-callback.php', $permissions);
+$fb = new Facebook\Facebook(array('app_id' => "ID", 'app_secret' => "SECRET", 'default_graph_version' => 'v2.5'));
+$helper = $fb->getRedirectLoginHelper();
 
- echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>'; 
+if(isset($_GET['code'])){
+  try {
+    $access_token = $helper->getAccessToken();
+  } catch(Facebook\Exceptions\FacebookResponseException $e) {
+    echo 'Graph returned an error: ' . $e->getMessage();
+    exit;
+  } catch(Facebook\Exceptions\FacebookSDKException $e) {
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+    exit;
+  }
+
+  if (isset($access_token)) {
+    var_dump($fb->get('/me/accounts', $access_token));
+  }
+} else {
+  $this_url = (empty($_SERVER['HTTPS'])?'http://':'https://').$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+  $permissions = array('manage_pages', 'publish_pages');
+  $login_url = $helper->getLoginUrl($this_url, $permissions);
+  echo '<a href="' . $login_url . '">Log in with Facebook!</a>';
+}
 ?>
