ea23860b6e2e3d01c963e7b399b809d8902583bb
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-10-04 16:07:32 +0000

    Pushed from Dropbox

---
+++ fb_regster.php
@@ -0,0 +1,8 @@
+<?php 
+	if(!isset($_COOKIE["userid"])){
+		$usr_id = $_COOKIE["userid"];
+		echo $usr_id;
+	}else{
+		echo "不正なアクセスです";
+	}
+ ?>
--- index.php
+++ index.php
@@ -91,12 +91,43 @@
 		console.log('Welcome!  Fetching your information.... ');
 		FB.api('/me', function(response) {
 			console.log('Successful login for: ' + response.name);
-			
 			document.getElementById('status').innerHTML =
 				'Thanks for logging in, ' + response.gender + '!';
 			});
-			
+
+			//idのCookieを保持してphpファイルに飛ばす
+			setCookie(userid,response.id,1);
+			window.location.replace('fb_regster.php');
 		}
+
+	//Cookieの生成
+		function setCookie(c_name,value,expiredays){
+    	// pathの指定
+    	var path = location.pathname;
+    	// pathをフォルダ毎に指定する場合のIE対策
+    	var paths = new Array();
+    	paths = path.split("/");
+    	if(paths[paths.length-1] != ""){
+     	   paths[paths.length-1] = "";
+    	    path = paths.join("/");
+    	}
+    	// 有効期限の日付
+    	var extime = new Date().getTime();
+    	var cltime = new Date(extime + (60*60*24*1000*expiredays));
+    	var exdate = cltime.toUTCString();
+    	// クッキーに保存する文字列を生成
+    	var s="";
+    	s += c_name +"="+ escape(value);// 値はエンコードしておく
+    	s += "; path="+ path;
+    	if(expiredays){
+        s += "; expires=" +exdate+"; ";
+    	}else{
+     	   s += "; ";
+    	}
+    	// クッキーに保存
+    	document.cookie=s;
+		}
+
 	</script>
 	<h3>ログインページ</h3>
 	<!--
