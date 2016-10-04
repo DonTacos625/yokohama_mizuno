401b00d29b0343fbbc5b8d8ab4ddd5b7cc44a181
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-10-04 16:50:42 +0000

    Pushed from Dropbox

--- fb_regster.php
+++ fb_regster.php
@@ -1,7 +1,11 @@
 <?php 
 	if(isset($_COOKIE["userid"])){
-		$usr_id = $_COOKIE["userid"];
+		$usr_id =$_COOKIE["userid"];
 		echo $usr_id;
+		setcookie("userid", $usr_id, time() - 1800); //Cookieの削除
+		if(!isset($_COOKIE["userid"])){
+			echo "正常";
+		}
 	}else{
 		echo "不正なアクセスです";
 	}
