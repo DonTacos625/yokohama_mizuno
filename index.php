4e09dbe6832cbd3db9fa5c3bae3ce68c544dfb12
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-10-11 19:14:00 +0000

    Pushed from Dropbox

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
 </head>
 <body>
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	<script type="text/javascript" src="jquery-3.1.1.min.js"></script>
 	<!--fecebookを使ったログイン-->
 	<script>
 	// This is called with the results from from FB.getLoginStatus().
