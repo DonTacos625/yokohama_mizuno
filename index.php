df1cb6825cded9668b248a31017328bdd328a741
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-10-11 18:57:17 +0000

    Pushed from Dropbox

--- index.php
+++ index.php
@@ -112,6 +112,7 @@
 	}
 
 	function connection(){
+		document.getElementById('status').innerHTML = 'Thanks for logging in, ';
 		$.ajax({
             url: 'api.php',
             type: 'post', // getかpostを指定(デフォルトは前者)
