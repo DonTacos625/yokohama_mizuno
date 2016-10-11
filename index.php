eb1ac249ea25ad127453a0c9685be4f41804c225
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-10-11 20:51:48 +0000

    Pushed from Dropbox

--- index.php
+++ index.php
@@ -39,17 +39,10 @@
 		} else if (response.status === 'not_authorized') {
 			// The person is logged into Facebook, but not your app.
 			document.getElementById('status').innerHTML = 'Please log ' + 'into this app.';
-					// Logged into your app and Facebook.
-			testAPI();
-			//Ajaxを使った通信
-			connection();
 		} else {
 			// The person is not logged into Facebook, so we're not sure if
 			// they are logged into this app or not.
-			document.getElementById('status').innerHTML = 'Please log ' + 'into Facebook.';		// Logged into your app and Facebook.
-			testAPI();
-			//Ajaxを使った通信
-			connection();
+			document.getElementById('status').innerHTML = 'Please log ' + 'into Facebook.';
 		}
 	}
 
