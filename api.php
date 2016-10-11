13b18142aea6e1d94a87679a4ff912cdfa06080b
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-10-11 21:10:59 +0000

    Pushed from Dropbox

--- api.php
+++ api.php
@@ -45,10 +45,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 		$pgsql->query($sql);
 		$error = "登録が完了しました";
 		$_SESSION["my_id"] = $usr_id;
-
-		//エラーメッセージ表示用
-		http_response_code(400);
-		echo json_encode(compact('error'));
+				echo json_encode(compact('error'));
 	}else{
 		//エラーメッセージ表示用
 		http_response_code(400);
