a5c7b7ea5fcac1a17ff7fe08536021cb88a179a5
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-10-15 10:05:47 +0000

    Pushed from Dropbox

--- api.php
+++ api.php
@@ -54,10 +54,10 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 		$_SESSION["my_no"] = $row['no'];
 //		$msg = "登録が完了しました.";
 //		echo json_encode(compact('msg'));
-		if(isset($_SESSION["my_no"])){
-			header("Location:./fb_regster.php");
-			exit;
-		}
+		// if(!empty($usr_id)){
+		// 	header("Location:./fb_regster.php");
+		// 	exit;
+		// }
 	}else{
 		//エラーメッセージ表示用
 		http_response_code(400);
