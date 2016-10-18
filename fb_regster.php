c1b038075ea53a929b53d51922a07ccaba9c6e96
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-10-18 10:02:56 +0000

    Pushed from Dropbox

--- fb_regster.php
+++ fb_regster.php
@@ -1,5 +1,6 @@
 <?php
 session_start(); //セッションスタート
+	echo $_SESSION["my_no"];
 //======================================================================
 //  ■： 会員情報登録ページ pwハッシュ化
 //======================================================================
@@ -57,24 +58,24 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 ?>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>ユーザ詳細情報登録</title>
-<link rel="stylesheet" type="text/css" href="stylet.css"></link>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>ユーザ詳細情報登録</title>
+	<link rel="stylesheet" type="text/css" href="stylet.css"></link>
 </head>
 <body>
-<?php
+	<?php
 	//----------------------------------------	
 	// ■ エラーメッセージがあったら表示
 	//----------------------------------------	
-if(strlen($access_error)>0){
-	echo $access_error;
-	echo "</body></html>";
-	exit;
-}
-if (strlen($error)>0){
-	if($error != "登録が完了しました."){
-		echo "<font size=\"6\" color=\"#da0b00\">{$error}</font><p>";
-	}else{
+	if(strlen($access_error)>0){
+		echo $access_error;
+		echo "</body></html>";
+		exit;
+	}
+	if (strlen($error)>0){
+		if($error != "登録が完了しました."){
+			echo "<font size=\"6\" color=\"#da0b00\">{$error}</font><p>";
+		}else{
 		header("Location: ./top.php"); //トップページへ(ゆくゆくはindex.php)
 		exit;
 	}
@@ -90,19 +91,19 @@ if (strlen($error)>0){
 	<div id="contents">
 		<!-- #main 本文スペース -->
 		<div class="contentswrap">
-		<form action="<?=$_SERVER["PHP_SELF"]?>" method="POST">
-			<table align="center" border="0" cellspacing="3" cellpadding="3"  width="600px">
-			<tr><div class="label" align="center">個人ステータスの登録</div></tr>
-			<tr><td align="center" bgcolor="#ffe4e1"><div class="label">性別</div></td>
-				<td>
-					<input type="radio" name="sex" value="1"<?php if ($sex==1){ print " checked"; }?> >男
-					<input type="radio" name="sex" value="2"<?php if ($sex==2){ print " checked"; }?> >女
-				</td>
-			</tr>
-			<tr>
-				<td align="center" bgcolor="#ffe4e1">
-					<div class="label">年代</div>
-				</td>
+			<form action="<?=$_SERVER["PHP_SELF"]?>" method="POST">
+				<table align="center" border="0" cellspacing="3" cellpadding="3"  width="600px">
+					<tr><div class="label" align="center">個人ステータスの登録</div></tr>
+					<tr><td align="center" bgcolor="#ffe4e1"><div class="label">性別</div></td>
+						<td>
+							<input type="radio" name="sex" value="1"<?php if ($sex==1){ print " checked"; }?> >男
+							<input type="radio" name="sex" value="2"<?php if ($sex==2){ print " checked"; }?> >女
+						</td>
+					</tr>
+					<tr>
+						<td align="center" bgcolor="#ffe4e1">
+							<div class="label">年代</div>
+						</td>
 						<td>
 							<input type="radio" name="age" value="10"<?php if ($age==10){ print " checked"; }?> >10
 							<input type="radio" name="age" value="20"<?php if ($age==20){ print " checked"; }?> >20
@@ -111,16 +112,16 @@ if (strlen($error)>0){
 							<input type="radio" name="age" value="50"<?php if ($age==50){ print " checked"; }?> >50
 							<input type="radio" name="age" value="60"<?php if ($age==60){ print " checked"; }?> >60以上
 						</td>
-			</tr>
-			</table>
+					</tr>
+				</table>
 			</table>
 			<br>
 			<table align="center" border="0" cellspacing="3" cellpadding="3"  width="600px">
-			<tr><div class="label2" align="center">嗜好情報の入力</div></tr>
-			<tr>
-				<td align="center" bgcolor="#ffe4e1">
-				<div class="label2">嗜好情報1</div>
-				</td>
+				<tr><div class="label2" align="center">嗜好情報の入力</div></tr>
+				<tr>
+					<td align="center" bgcolor="#ffe4e1">
+						<div class="label2">嗜好情報1</div>
+					</td>
 					<td>
 						<input type="radio" name="a1" value="1"<?php if ($a1==1){ print " checked"; }?> >1
 						<input type="radio" name="a1" value="2"<?php if ($a1==2){ print " checked"; }?> >2
@@ -128,11 +129,11 @@ if (strlen($error)>0){
 						<input type="radio" name="a1" value="4"<?php if ($a1==4){ print " checked"; }?> >4
 						<input type="radio" name="a1" value="5"<?php if ($a1==5){ print " checked"; }?> >5
 					</td>
-			</tr>
-			<tr>
-				<td align="center" bgcolor="#ffe4e1">
-				<div class="label2">嗜好情報2</div>
-				</td>
+				</tr>
+				<tr>
+					<td align="center" bgcolor="#ffe4e1">
+						<div class="label2">嗜好情報2</div>
+					</td>
 					<td>
 						<input type="radio" name="a2" value="1"<?php if ($a2==1){ print " checked"; }?> >1
 						<input type="radio" name="a2" value="2"<?php if ($a2==2){ print " checked"; }?> >2
@@ -140,11 +141,11 @@ if (strlen($error)>0){
 						<input type="radio" name="a2" value="4"<?php if ($a2==4){ print " checked"; }?> >4
 						<input type="radio" name="a2" value="5"<?php if ($a2==5){ print " checked"; }?> >5
 					</td>
-			</tr>
-			<tr>
-				<td align="center" bgcolor="#ffe4e1">
-				<div class="label2">嗜好情報3</div>
-				</td>
+				</tr>
+				<tr>
+					<td align="center" bgcolor="#ffe4e1">
+						<div class="label2">嗜好情報3</div>
+					</td>
 					<td>
 						<input type="radio" name="a3" value="1"<?php if ($a3==1){ print " checked"; }?> >1
 						<input type="radio" name="a3" value="2"<?php if ($a3==2){ print " checked"; }?> >2
@@ -152,11 +153,11 @@ if (strlen($error)>0){
 						<input type="radio" name="a3" value="4"<?php if ($a3==4){ print " checked"; }?> >4
 						<input type="radio" name="a3" value="5"<?php if ($a3==5){ print " checked"; }?> >5
 					</td>
-			</tr>
-			<tr>
-				<td align="center" bgcolor="#ffe4e1">
-				<div class="label2">嗜好情報4</div>
-				</td>
+				</tr>
+				<tr>
+					<td align="center" bgcolor="#ffe4e1">
+						<div class="label2">嗜好情報4</div>
+					</td>
 					<td>
 						<input type="radio" name="a4" value="1"<?php if ($a4==1){ print " checked"; }?> >1
 						<input type="radio" name="a4" value="2"<?php if ($a4==2){ print " checked"; }?> >2
@@ -164,11 +165,11 @@ if (strlen($error)>0){
 						<input type="radio" name="a4" value="4"<?php if ($a4==4){ print " checked"; }?> >4
 						<input type="radio" name="a4" value="5"<?php if ($a4==5){ print " checked"; }?> >5
 					</td>
-			</tr>
-			<tr>
-				<td align="center" bgcolor="#ffe4e1">
-				<div class="label2">嗜好情報5</div>
-				</td>
+				</tr>
+				<tr>
+					<td align="center" bgcolor="#ffe4e1">
+						<div class="label2">嗜好情報5</div>
+					</td>
 					<td>
 						<input type="radio" name="a5" value="1"<?php if ($a5==1){ print " checked"; }?> >1
 						<input type="radio" name="a5" value="2"<?php if ($a5==2){ print " checked"; }?> >2
@@ -176,11 +177,11 @@ if (strlen($error)>0){
 						<input type="radio" name="a5" value="4"<?php if ($a5==4){ print " checked"; }?> >4
 						<input type="radio" name="a5" value="5"<?php if ($a5==5){ print " checked"; }?> >5
 					</td>
-			</tr>
-			<tr>
-				<td align="center" bgcolor="#ffe4e1">
-				<div class="label2">嗜好情報6</div>
-				</td>
+				</tr>
+				<tr>
+					<td align="center" bgcolor="#ffe4e1">
+						<div class="label2">嗜好情報6</div>
+					</td>
 					<td>
 						<input type="radio" name="a6" value="1"<?php if ($a6==1){ print " checked"; }?> >1
 						<input type="radio" name="a6" value="2"<?php if ($a6==2){ print " checked"; }?> >2
@@ -188,11 +189,11 @@ if (strlen($error)>0){
 						<input type="radio" name="a6" value="4"<?php if ($a6==4){ print " checked"; }?> >4
 						<input type="radio" name="a6" value="5"<?php if ($a6==5){ print " checked"; }?> >5
 					</td>
-			</tr>
-			<tr>
-				<td align="center" bgcolor="#ffe4e1">
-				<div class="label2">嗜好情報7</div>
-				</td>
+				</tr>
+				<tr>
+					<td align="center" bgcolor="#ffe4e1">
+						<div class="label2">嗜好情報7</div>
+					</td>
 					<td>
 						<input type="radio" name="a7" value="1"<?php if ($a7==1){ print " checked"; }?> >1
 						<input type="radio" name="a7" value="2"<?php if ($a7==2){ print " checked"; }?> >2
@@ -200,11 +201,11 @@ if (strlen($error)>0){
 						<input type="radio" name="a7" value="4"<?php if ($a7==4){ print " checked"; }?> >4
 						<input type="radio" name="a7" value="5"<?php if ($a7==5){ print " checked"; }?> >5
 					</td>
-			</tr>
-			<tr>
-				<td align="center" bgcolor="#ffe4e1">
-				<div class="label2">嗜好情報8</div>
-				</td>
+				</tr>
+				<tr>
+					<td align="center" bgcolor="#ffe4e1">
+						<div class="label2">嗜好情報8</div>
+					</td>
 					<td>
 						<input type="radio" name="a8" value="1"<?php if ($a8==1){ print " checked"; }?> >1
 						<input type="radio" name="a8" value="2"<?php if ($a8==2){ print " checked"; }?> >2
@@ -212,11 +213,11 @@ if (strlen($error)>0){
 						<input type="radio" name="a8" value="4"<?php if ($a8==4){ print " checked"; }?> >4
 						<input type="radio" name="a8" value="5"<?php if ($a8==5){ print " checked"; }?> >5
 					</td>
-			</tr>
-			<tr>
-				<td align="center" bgcolor="#ffe4e1">
-				<div class="label2">嗜好情報9</div>
-				</td>
+				</tr>
+				<tr>
+					<td align="center" bgcolor="#ffe4e1">
+						<div class="label2">嗜好情報9</div>
+					</td>
 					<td>
 						<input type="radio" name="a9" value="1"<?php if ($a9==1){ print " checked"; }?> >1
 						<input type="radio" name="a9" value="2"<?php if ($a9==2){ print " checked"; }?> >2
@@ -224,11 +225,11 @@ if (strlen($error)>0){
 						<input type="radio" name="a9" value="4"<?php if ($a9==4){ print " checked"; }?> >4
 						<input type="radio" name="a9" value="5"<?php if ($a9==5){ print " checked"; }?> >5
 					</td>
-			</tr>
-			<tr>
-				<td align="center" bgcolor="#ffe4e1">
-				<div class="label2">嗜好情報10</div>
-				</td>
+				</tr>
+				<tr>
+					<td align="center" bgcolor="#ffe4e1">
+						<div class="label2">嗜好情報10</div>
+					</td>
 					<td>
 						<input type="radio" name="a10" value="1"<?php if ($a10==1){ print " checked"; }?> >1
 						<input type="radio" name="a10" value="2"<?php if ($a10==2){ print " checked"; }?> >2
@@ -236,11 +237,11 @@ if (strlen($error)>0){
 						<input type="radio" name="a10" value="4"<?php if ($a10==4){ print " checked"; }?> >4
 						<input type="radio" name="a10" value="5"<?php if ($a10==5){ print " checked"; }?> >5
 					</td>
-			</tr>
-			<tr>
-				<td align="center" bgcolor="#ffe4e1">
-				<div class="label2">嗜好情報11</div>
-				</td>
+				</tr>
+				<tr>
+					<td align="center" bgcolor="#ffe4e1">
+						<div class="label2">嗜好情報11</div>
+					</td>
 					<td>
 						<input type="radio" name="a11" value="1"<?php if ($a11==1){ print " checked"; }?> >1
 						<input type="radio" name="a11" value="2"<?php if ($a11==2){ print " checked"; }?> >2
@@ -248,11 +249,11 @@ if (strlen($error)>0){
 						<input type="radio" name="a11" value="4"<?php if ($a11==4){ print " checked"; }?> >4
 						<input type="radio" name="a11" value="5"<?php if ($a11==5){ print " checked"; }?> >5
 					</td>
-			</tr>
-			<tr><td align="center" colspan="1">
-			<input type="submit" name="Submit" value="登録する"></td></tr>
-			</table>
-		</form>
+				</tr>
+				<tr><td align="center" colspan="1">
+					<input type="submit" name="Submit" value="登録する"></td></tr>
+				</table>
+			</form>
 		</div>
 	</div>
 </div>
