9931e08d0de195b4e4daf416bacbb6e92f0d4895
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-10-18 18:36:58 +0000

    Pushed from Dropbox

--- test.php
+++ test.php
@@ -3,9 +3,10 @@
 	$tmp = 0;
 	if(strlen($row[1])!=0){
 		for($i=1;i<6;i++){
-			if(strlen($row[i])==0)
+			if(strlen($row[$i])==0)
 				break;
-			$tmp = $tmp + $row[i];
+			//$tmp = $tmp + $row[i];
+			echo $row[$i];
 		}
 	}
 	echo $tmp;
