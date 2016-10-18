e445fb85bbfaf2c13fedae021305d39f6b7df306
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-10-18 03:42:49 +0000

    Pushed from Dropbox

--- login_submit.php
+++ login_submit.php
@@ -59,13 +59,13 @@ if ($_SERVER["REQUEST_METHOD"]=="POST"){
 	if (strlen($usr_id)==0){
 		$error = "ユーザIDが入力されていません";
 		echo $error.$br;
-		echo $login_html+"へ";
+		echo $login_html."へ";
 	}
 	//パスワード
 	if (strlen($usr_pw)==0){
 		$error = "パスワードが入力されていません";
 		echo $error.$br;
-		echo $login_html+"へ";
+		echo $login_html."へ";
 	}
 	//エラーなし
 	if (strlen($error)==NULL){
@@ -79,7 +79,7 @@ if ($_SERVER["REQUEST_METHOD"]=="POST"){
 				$_SESSION["my_no"] = $row["no"];
 				if(!isset($row["sex"])){
 					echo "１回目のログインです.";
-					echo $regist_html+"より詳細な会員情報入力をお願いします."
+					echo $regist_html."より詳細な会員情報入力をお願いします."
 
 				}
 				//$_SESSION["my_id"] = $usr_id;
