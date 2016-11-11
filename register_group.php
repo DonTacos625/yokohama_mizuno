1c1ff2e2891aea594cb4045db9c9c9eb58902122
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-11-11 14:06:24 +0000

    Pushed from Dropbox

--- register_group.php
+++ register_group.php
@@ -318,8 +318,12 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
 		$sql = "SELECT a1,a2,a3,a4,a5,a6,a7,a8 FROM friendinfo where no in('$my_no','$f1','$f2','$f3')";
 		$pgsql->query($sql);
-		$databox = $pgsql->fetch_all();
-
+		$rows = $pgsql->fetch_all();
+		for($i=0;$i<$rows;$i++){
+			for($j=0;$j<$rows[0];$j++){
+				$databox[$i][$j]=$rows[$i][$j];
+			}
+		}
 		$family=value_calcuation($databox);
 		var_dump($databox);
 		var_dump($family);
