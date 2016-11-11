32872e07448047490897a817a8106319e061452c
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-11-11 14:09:05 +0000

    Pushed from Dropbox

--- register_group.php
+++ register_group.php
@@ -319,7 +319,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 		$sql = "SELECT a1,a2,a3,a4,a5,a6,a7,a8 FROM friendinfo where no in('$my_no','$f1','$f2','$f3')";
 		$pgsql->query($sql);
 		$rows = $pgsql->fetch_all();
-		echo $rows[0][1];
+		echo $rows[0]["a1"];
 		/*for($i=0;$i<$rows;$i++){
 			for($j=0;$j<$rows[0];$j++){
 				$databox[$i][$j]=$rows[$i][$j];
