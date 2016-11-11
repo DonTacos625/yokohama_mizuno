367ebe5ef220e057c329025dc295c69b446cbab2
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-11-11 10:56:56 +0000

    Pushed from Dropbox

--- index.php
+++ index.php
@@ -4,13 +4,14 @@
 //======================================================================
 	/*
 		セッション管理
-	*/
-		/*
+	
+
 		session_start();
 		if(isset($_SESSION["my_no"])){
 			header("Location:./top.php");
 			exit;
 		}
+
 		*/
 ?>
 	<html>
@@ -112,8 +113,6 @@
             	u_id: userid
             },
         success:function(){ //facebook初回ログイン
-        	//document.getElementById('status').innerHTML = "登録できた！";
-        	//alert("しっぱい！");
         	location.href = "./fb_regster.php"; //facebook初回ログイン登録用
         	exit;
         },
