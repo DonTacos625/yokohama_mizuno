a10966a0698e4ac7f43adbea1e3116c8e51d7914
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-11-15 06:51:02 +0000

    Pushed from Dropbox

--- login_submit.php
+++ login_submit.php
@@ -80,7 +80,7 @@ if ($_SERVER["REQUEST_METHOD"]=="POST"){
 					header("Location: ./index.php");
 					exit;
 				}else{
-					header("Location: ./fb_regster.php");
+					header("Location: ./fb_register.php");
 					exit;
 				}
 			}else{
