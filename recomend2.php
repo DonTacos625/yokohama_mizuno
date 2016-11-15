120fce524ef3832bd68f97b0edfc85ec2ae3a2d0
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-11-15 13:27:45 +0000

    Pushed from Dropbox

--- recomend2.php
+++ recomend2.php
@@ -218,13 +218,13 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
 			// ■ヘッダーの取り込み
 			//----------------------------------------
 		require_once("./header.php");
-		if($error!=NULL){
+		require_once("./linkplace.php");
+		echo pwd("recomend2");
+		if(strlen($error)!=0){
 			echo $error;
 			echo "</dvi></div></body></html>";
 			exit;
 		}
-		require_once("./linkplace.php");
-		echo pwd("recomend2");
 		?>
 	</div>
 	</div>
