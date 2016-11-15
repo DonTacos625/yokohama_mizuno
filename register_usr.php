3640f246c7054ba90ecdec430a5fec20b5a0e834
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-11-15 06:43:30 +0000

    Pushed from Dropbox

--- register_usr.php
+++ register_usr.php
@@ -12,7 +12,7 @@ $error1 = ""; //PW関係
 
 // POSTメソッドで送信された場合は書き込み処理を実行する
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-	$pgsql->query("SELECT MAX(no) AS no FROM friendinfo");
+	$pgsql->query_null("SELECT MAX(no) AS no FROM friendinfo");
 	if ($pgsql->rows()>0) {
 		$row = $pgsql->fetch();
 		$no = $row['no'];
