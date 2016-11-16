ecb7ff94b33fc54b314d077526dd83727385e86f
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-11-16 19:01:05 +0000

    Pushed from Dropbox

--- login.php
+++ login.php
@@ -5,13 +5,45 @@ require_once __DIR__ . '/vendor/autoload.php';
 $fb = new Facebook\Facebook([
   'app_id' => '783967058409220', // Replace {app-id} with your app id
   'app_secret' => 'ebec4c0671fb644b7d077913deea9229',
-  'default_graph_version' => 'v2.8',
+  'default_graph_version' => 'v2.2',
   ]);
 
-$helper = $fb->getRedirectLoginHelper();
+//$helper = $fb->getRedirectLoginHelper();
+
+$helper = $fb->getCanvasHelper();
+
+try {
+  $accessToken = $helper->getAccessToken();
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+  // When Graph returns an error
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+  // When validation fails or other local issues
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+}
+
+if (! isset($accessToken)) {
+  echo 'No OAuth data could be obtained from the signed request. User has not authorized your app yet.';
+  exit;
+}
+
+// Logged in
+echo '<h3>Signed Request</h3>';
+var_dump($helper->getSignedRequest());
+
+echo '<h3>Access Token</h3>';
+var_dump($accessToken->getValue());
+
+/*
+
+
+
+
 
 $permissions = ['email']; // Optional permissions
 $loginUrl = $helper->getLoginUrl('https://websitetest1234.herokuapp.com/fb-callback.php', $permissions);
 
-echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
+echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';*/
 ?>
