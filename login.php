82e53f133f165b1780a1079bbca65807d0fe3fad
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-11-16 19:05:55 +0000

    Pushed from Dropbox

--- login.php
+++ login.php
@@ -7,7 +7,7 @@ echo "おｋ";
 $fb = new Facebook\Facebook([
   'app_id' => '783967058409220', // Replace {app-id} with your app id
   'app_secret' => 'ebec4c0671fb644b7d077913deea9229',
-  'default_graph_version' => 'v2.2',
+  'default_graph_version' => 'v2.7',
   ]);
 
 //$helper = $fb->getRedirectLoginHelper();
