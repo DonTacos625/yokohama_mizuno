66c45795b142b53516659d03ff59196b1ba2d307
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-11-16 19:04:19 +0000

    Pushed from Dropbox

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 echo "おｋ";
 
-/*$fb = new Facebook\Facebook([
+$fb = new Facebook\Facebook([
   'app_id' => '783967058409220', // Replace {app-id} with your app id
   'app_secret' => 'ebec4c0671fb644b7d077913deea9229',
   'default_graph_version' => 'v2.2',
@@ -14,6 +14,9 @@ echo "おｋ";
 
 $helper = $fb->getCanvasHelper();
 
+echo "おｋ";
+
+/*
 try {
   $accessToken = $helper->getAccessToken();
 } catch(Facebook\Exceptions\FacebookResponseException $e) {
