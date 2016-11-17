c5f2b309a956279711c275c94c775fb127473b41
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-11-17 06:57:28 +0000

    Pushed from Dropbox

--- local_evaluation.php
+++ local_evaluation.php
@@ -1,89 +1,89 @@
 <?php
-	session_start();
-	require_once('PostgreSQL.php');
-	require_once('calcuation.php');
-	$pgsql = new PostgreSQL;
+session_start();
+require_once('PostgreSQL.php');
+require_once('calcuation.php');
+$pgsql = new PostgreSQL;
 
 // pphpの配列をpostgresqlの配列に変換
-private function toPostgreSqlArray($data)
+function toPostgreSqlArray($data)
 {   
-    return '{' . implode(',', $data) . '}';
+	return '{' . implode(',', $data) . '}';
 }   
 
 // postgresの配列をphpの配列に変換
-private function toPhpArray($data)
+function toPhpArray($data)
 {
-    $data       = str_replace('{', '', $data);
-    $data       = str_replace('}', '', $data);
-    $array_data = explode(',', $data);
+	$data       = str_replace('{', '', $data);
+	$data       = str_replace('}', '', $data);
+	$array_data = explode(',', $data);
 
-    return $array_data;
+	return $array_data;
 }
-	if ($_SERVER['REQUEST_METHOD'] == "POST") {
-		$a1 = intval(htmlspecialchars($_POST['a1']));
-		$a2 = intval(htmlspecialchars($_POST['a2']));
-		$a3 = intval(htmlspecialchars($_POST['a3']));
-		$a4 = intval(htmlspecialchars($_POST['a4']));
-		$a5 = intval(htmlspecialchars($_POST['a5']));
-		$a6 = intval(htmlspecialchars($_POST['a6']));
-		$a7 = intval(htmlspecialchars($_POST['a7']));
-		$a8 = intval(htmlspecialchars($_POST['a8']));
+if ($_SERVER["REQUEST_METHOD"]=="POST") {
+	$a1 = intval(htmlspecialchars($_POST['a1']));
+	$a2 = intval(htmlspecialchars($_POST['a2']));
+	$a3 = intval(htmlspecialchars($_POST['a3']));
+	$a4 = intval(htmlspecialchars($_POST['a4']));
+	$a5 = intval(htmlspecialchars($_POST['a5']));
+	$a6 = intval(htmlspecialchars($_POST['a6']));
+	$a7 = intval(htmlspecialchars($_POST['a7']));
+	$a8 = intval(htmlspecialchars($_POST['a8']));
 
-		if($a1<6&&$a2<6&&$a3<6&&$a4<6&&$a5<6&&$a6<6&&$a7<6&&$a8<6){
-			$sql = "SELECT spot_a1,spot_a2,spot_a3,spot_a4,spot_a5,spot_a6,spot_a7,spot_a8,spot_visited FROM localinfo WHERE pk = $1";
-			$array = array($pk);
-			$pgsql -> query($sql,$array);
-			$row = $pgsql->fetch();
+	if($a1<6&&$a2<6&&$a3<6&&$a4<6&&$a5<6&&$a6<6&&$a7<6&&$a8<6){
+		$sql = "SELECT spot_a1,spot_a2,spot_a3,spot_a4,spot_a5,spot_a6,spot_a7,spot_a8,spot_visited FROM localinfo WHERE pk = $1";
+		$array = array($pk);
+		$pgsql -> query($sql,$array);
+		$row = $pgsql->fetch();
 
-			if($row["spot_a1"]==0){
-				$data[0][0]= $a1;
-			}else{
-				$data[0][0]= intval($row["spot_a1"]);
-			}
-			if($row["spot_a2"]==0){
-				$data[0][1]= $a2;
-			}else{
-				$data[0][1]= intval($row["spot_a2"]);
-			}
-			if($row["spot_a3"]==0){
-				$data[0][2]= $a3;
-			}else{
-				$data[0][2]= intval($row["spot_a3"]);
-			}
-			if($row["spot_a4"]==0){
-				$data[0][3]= $a4;
-			}else{
-				$data[0][3]= intval($row["spot_a4"]);
-			}
-			if($row["spot_a5"]==0){
-				$data[0][4]= $a5;
-			}else{
-				$data[0][4]= intval($row["spot_a5"]);
-			}
-			if($row["spot_a6"]==0){
-				$data[0][5]= $a6;
-			}else{
-				$data[0][5]= intval($row["spot_a6"]);
-			}
-			if($row["spot_a7"]==0){
-				$data[0][6]= $a7;
-			}else{
-				$data[0][6]= intval($row["spot_a7"]);
-			}
-			if($row["spot_a8"]==0){
-				$data[0][7]= $a8;
-			}else{
-				$data[0][7]= intval($row["spot_a8"]);
-			}
+		if($row["spot_a1"]==0){
+			$data[0][0]= $a1;
+		}else{
+			$data[0][0]= intval($row["spot_a1"]);
+		}
+		if($row["spot_a2"]==0){
+			$data[0][1]= $a2;
+		}else{
+			$data[0][1]= intval($row["spot_a2"]);
+		}
+		if($row["spot_a3"]==0){
+			$data[0][2]= $a3;
+		}else{
+			$data[0][2]= intval($row["spot_a3"]);
+		}
+		if($row["spot_a4"]==0){
+			$data[0][3]= $a4;
+		}else{
+			$data[0][3]= intval($row["spot_a4"]);
+		}
+		if($row["spot_a5"]==0){
+			$data[0][4]= $a5;
+		}else{
+			$data[0][4]= intval($row["spot_a5"]);
+		}
+		if($row["spot_a6"]==0){
+			$data[0][5]= $a6;
+		}else{
+			$data[0][5]= intval($row["spot_a6"]);
+		}
+		if($row["spot_a7"]==0){
+			$data[0][6]= $a7;
+		}else{
+			$data[0][6]= intval($row["spot_a7"]);
+		}
+		if($row["spot_a8"]==0){
+			$data[0][7]= $a8;
+		}else{
+			$data[0][7]= intval($row["spot_a8"]);
+		}
 
-			$data[1][0] = $a1;
-			$data[1][1] = $a2;
-			$data[1][2] = $a3;
-			$data[1][3] = $a4;
-			$data[1][4] = $a5;
-			$data[1][5] = $a6;
-			$data[1][6] = $a7;
-			$data[1][7] = $a8;
+		$data[1][0] = $a1;
+		$data[1][1] = $a2;
+		$data[1][2] = $a3;
+		$data[1][3] = $a4;
+		$data[1][4] = $a5;
+		$data[1][5] = $a6;
+		$data[1][6] = $a7;
+		$data[1][7] = $a8;
 
 			$resultval = value_calcuation($data); //データの計算
 			$visited = intval($row["spot_visited"])+1; //訪問者を一人増やす
@@ -129,138 +129,138 @@ private function toPhpArray($data)
 			$error = "ログインをお願いします";
 		}
 	}
-?>
-<!DOCTYPE html "-//W3C//DTD XHTML 1.0 Strict//EN" 
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-
-<head>
-<meta http-equiv="Content-type" content="text/html; charset=utf-8"></meta>
-<title>地域情報</title>
-<link rel="stylesheet" type="text/css" href="stylet.css"></link>
-</head>
-
-<body>
-<div id="page">
-	<?php
-		require_once('header.php');
-		if(strlen($error)!=0){
-			echo $error;
-			echo "</div></body></html>";
-			exit;
-		}
 	?>
-	<div id="contents">
-		<?php
-		require_once('left.php');
-		?>
-		<div id="main">
-			<div class="contentswrap">
-				<form action="<?php $_SERVER["PHP_SELF"]?>" method="post">
-				<table border="0" cellspacing="3" cellpadding="3" width="600"  >
-				<tr><td align="center" bgcolor="#fof8ff" colspan="2">
-				<font size="4"><b>観光スポットの評価情報を投稿する</b></font></td></tr>
-				<td align="center" bgcolor="#fof8ff">
-				<font size="4"><b>スポット名</b></font></td>
-				<td><?php echo  $spot_name?></td>
-				<tr><td align="center" bgcolor="#fof8ff"><font size="4"><b>カテゴリー</b></font></td>
-				<td>
-					<?php
-					if($spot_category==1){
-						echo "飲食";
-					}else if($spot_category==2){
-						echo "ショッピング";
-					}else if($spot_category==3){
-						echo "テーマパーク・公園";
-					}else if($spot_category==4){
-						echo "名所・史跡";
-					}else if($spot_category==5){
-						echo "芸術・博物館";
-					}else{
-						echo "その他";
-					}
-					?>
-				</td>
-				</tr>
-				<tr><?echo $spot_pic?></tr>
-			<tr><td align="center" bgcolor="#fof8ff"><font size="4"><b>評価</b></font></td>
-			<td>1:低/少  <------>  5:高/多</td>
-			</tr>
-			<tr><td align="center" bgcolor="#fof8ff"><b>満足度</b></td>
-						<td>
-							<input type="radio" name="a1" value="1" checked> 1
-							<input type="radio" name="a1" value="2"> 2
-							<input type="radio" name="a1" value="3"> 3
-							<input type="radio" name="a1" value="4"> 4
-							<input type="radio" name="a1" value="5"> 5
-						</td>
-			</tr>
-			<tr><td align="center" bgcolor="#fof8ff"><b>アクセス</b></td>
-						<<td>
-							<input type="radio" name="a2" value="1" checked> 1
-							<input type="radio" name="a2" value="2"> 2
-							<input type="radio" name="a2" value="3"> 3
-							<input type="radio" name="a2" value="4"> 4
-							<input type="radio" name="a2" value="5"> 5
-						</td>
-			</tr>
-			<tr><td align="center" bgcolor="#fof8ff"><b>人ごみの少なさ</b></td>
-						<td>
-							<input type="radio" name="a3" value="1" checked> 1
-							<input type="radio" name="a3" value="2"> 2
-							<input type="radio" name="a3" value="3"> 3
-							<input type="radio" name="a3" value="4"> 4
-							<input type="radio" name="a3" value="5"> 5
-						</td>
-			</tr>
-			<tr><td align="center" bgcolor="#fof8ff"><b>バリアフリー</b></td>
-						<td>
-							<input type="radio" name="a4" value="1" checked> 1
-							<input type="radio" name="a4" value="2"> 2
-							<input type="radio" name="a4" value="3"> 3
-							<input type="radio" name="a4" value="4"> 4
-							<input type="radio" name="a4" value="5"> 5
-						</td>
-			</tr>
-			<tr><td align="center" bgcolor="#fof8ff"><b>コストパフォーマンス</b></td>
-						<td>
-							<input type="radio" name="a5" value="1" checked> 1
-							<input type="radio" name="a5" value="2"> 2
-							<input type="radio" name="a5" value="3"> 3
-							<input type="radio" name="a5" value="4"> 4
-							<input type="radio" name="a5" value="5"> 5
-						</td>
-			</tr>
-			<tr><td align="center" bgcolor="#fof8ff"><b>雰囲気</b></td>
-						<td>
-							<input type="radio" name="a6" value="1" checked> 1
-							<input type="radio" name="a6" value="2"> 2
-							<input type="radio" name="a6" value="3"> 3
-							<input type="radio" name="a6" value="4"> 4
-							<input type="radio" name="a6" value="5"> 5
-						</td>
-			</tr>
-			<tr><td align="center" bgcolor="#fof8ff"><b>快適度/サービスの良さ</b></td>
-						<td>
-							<input type="radio" name="a7" value="1" checked> 1
-							<input type="radio" name="a7" value="2"> 2
-							<input type="radio" name="a7" value="3"> 3
-							<input type="radio" name="a7" value="4"> 4
-							<input type="radio" name="a7" value="5"> 5
-						</td>
-			</tr>
-			<tr><td align="center" bgcolor="#fof8ff"><b>おすすめ度</b></td>
-						<td>
-							<input type="radio" name="a8" value="1" checked> 1
-							<input type="radio" name="a8" value="2"> 2
-							<input type="radio" name="a8" value="3"> 3
-							<input type="radio" name="a8" value="4"> 4
-							<input type="radio" name="a8" value="5"> 5
-						</td>
-			</tr>
-				</table>
-				<table border="0" cellspacing="3" cellpadding="3" width="600"  >
-				<tr><td align="center" colspan="2">
-				<input type="reset" name="submit_reset" value="リセット">
-				<input type="submit" name="submit_toko" value="投稿する" onClick="return confirm('この内容で投稿しますか？')">
-			</td></tr></table></form></div></div></div></div></body></html>
+	<!DOCTYPE html "-//W3C//DTD XHTML 1.0 Strict//EN" 
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+	<html xmlns="http://www.w3.org/1999/xhtml">
+
+	<head>
+		<meta http-equiv="Content-type" content="text/html; charset=utf-8"></meta>
+		<title>地域情報</title>
+		<link rel="stylesheet" type="text/css" href="stylet.css"></link>
+	</head>
+
+	<body>
+		<div id="page">
+			<?php
+			require_once('header.php');
+			if(strlen($error)!=0){
+				echo $error;
+				echo "</div></body></html>";
+				exit;
+			}
+			?>
+			<div id="contents">
+				<?php
+				require_once('left.php');
+				?>
+				<div id="main">
+					<div class="contentswrap">
+						<form action="<?php $_SERVER["PHP_SELF"]?>" method="post">
+							<table border="0" cellspacing="3" cellpadding="3" width="600"  >
+								<tr><td align="center" bgcolor="#fof8ff" colspan="2">
+									<font size="4"><b>観光スポットの評価情報を投稿する</b></font></td></tr>
+									<td align="center" bgcolor="#fof8ff">
+										<font size="4"><b>スポット名</b></font></td>
+										<td><?php echo  $spot_name?></td>
+										<tr><td align="center" bgcolor="#fof8ff"><font size="4"><b>カテゴリー</b></font></td>
+											<td>
+												<?php
+												if($spot_category==1){
+													echo "飲食";
+												}else if($spot_category==2){
+													echo "ショッピング";
+												}else if($spot_category==3){
+													echo "テーマパーク・公園";
+												}else if($spot_category==4){
+													echo "名所・史跡";
+												}else if($spot_category==5){
+													echo "芸術・博物館";
+												}else{
+													echo "その他";
+												}
+												?>
+											</td>
+										</tr>
+										<tr><?echo $spot_pic?></tr>
+										<tr><td align="center" bgcolor="#fof8ff"><font size="4"><b>評価</b></font></td>
+											<td>1:低/少  <------>  5:高/多</td>
+										</tr>
+										<tr><td align="center" bgcolor="#fof8ff"><b>満足度</b></td>
+											<td>
+												<input type="radio" name="a1" value="1" checked> 1
+												<input type="radio" name="a1" value="2"> 2
+												<input type="radio" name="a1" value="3"> 3
+												<input type="radio" name="a1" value="4"> 4
+												<input type="radio" name="a1" value="5"> 5
+											</td>
+										</tr>
+										<tr><td align="center" bgcolor="#fof8ff"><b>アクセス</b></td>
+											<<td>
+											<input type="radio" name="a2" value="1" checked> 1
+											<input type="radio" name="a2" value="2"> 2
+											<input type="radio" name="a2" value="3"> 3
+											<input type="radio" name="a2" value="4"> 4
+											<input type="radio" name="a2" value="5"> 5
+										</td>
+									</tr>
+									<tr><td align="center" bgcolor="#fof8ff"><b>人ごみの少なさ</b></td>
+										<td>
+											<input type="radio" name="a3" value="1" checked> 1
+											<input type="radio" name="a3" value="2"> 2
+											<input type="radio" name="a3" value="3"> 3
+											<input type="radio" name="a3" value="4"> 4
+											<input type="radio" name="a3" value="5"> 5
+										</td>
+									</tr>
+									<tr><td align="center" bgcolor="#fof8ff"><b>バリアフリー</b></td>
+										<td>
+											<input type="radio" name="a4" value="1" checked> 1
+											<input type="radio" name="a4" value="2"> 2
+											<input type="radio" name="a4" value="3"> 3
+											<input type="radio" name="a4" value="4"> 4
+											<input type="radio" name="a4" value="5"> 5
+										</td>
+									</tr>
+									<tr><td align="center" bgcolor="#fof8ff"><b>コストパフォーマンス</b></td>
+										<td>
+											<input type="radio" name="a5" value="1" checked> 1
+											<input type="radio" name="a5" value="2"> 2
+											<input type="radio" name="a5" value="3"> 3
+											<input type="radio" name="a5" value="4"> 4
+											<input type="radio" name="a5" value="5"> 5
+										</td>
+									</tr>
+									<tr><td align="center" bgcolor="#fof8ff"><b>雰囲気</b></td>
+										<td>
+											<input type="radio" name="a6" value="1" checked> 1
+											<input type="radio" name="a6" value="2"> 2
+											<input type="radio" name="a6" value="3"> 3
+											<input type="radio" name="a6" value="4"> 4
+											<input type="radio" name="a6" value="5"> 5
+										</td>
+									</tr>
+									<tr><td align="center" bgcolor="#fof8ff"><b>快適度/サービスの良さ</b></td>
+										<td>
+											<input type="radio" name="a7" value="1" checked> 1
+											<input type="radio" name="a7" value="2"> 2
+											<input type="radio" name="a7" value="3"> 3
+											<input type="radio" name="a7" value="4"> 4
+											<input type="radio" name="a7" value="5"> 5
+										</td>
+									</tr>
+									<tr><td align="center" bgcolor="#fof8ff"><b>おすすめ度</b></td>
+										<td>
+											<input type="radio" name="a8" value="1" checked> 1
+											<input type="radio" name="a8" value="2"> 2
+											<input type="radio" name="a8" value="3"> 3
+											<input type="radio" name="a8" value="4"> 4
+											<input type="radio" name="a8" value="5"> 5
+										</td>
+									</tr>
+								</table>
+								<table border="0" cellspacing="3" cellpadding="3" width="600"  >
+									<tr><td align="center" colspan="2">
+										<input type="reset" name="submit_reset" value="リセット">
+										<input type="submit" name="submit_toko" value="投稿する" onClick="return confirm('この内容で投稿しますか？')">
+									</td></tr></table></form></div></div></div></div></body></html>
