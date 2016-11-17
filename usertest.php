56c5b58bdc417b692b8313d9dad8a08ad472d559
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-11-17 04:37:51 +0000

    Pushed from Dropbox

--- usertest.php
+++ usertest.php
@@ -1,5 +1,10 @@
 <?php
-
-phpinfo();
+echo mb_substr('シーンライブ', 0, 4,"UTF-8");  // 実行結果９ : シーンラ
+ 
+echo mb_substr('シーンライブ', 0, 6,"UTF-8");  // 実行結果１０ : シーンライブ
+ 
+echo mb_substr('シーンライブ', -2, 2,"UTF-8"); // 実行結果１１ : イブ
+ 
+echo mb_substr('scenelive', 0, 4); // 実行結果１２ : scen
 
 ?>
