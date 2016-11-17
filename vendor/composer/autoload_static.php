f037b1e6c1873a72435ac0fd8f6f818b350c8339
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-11-17 04:49:01 +0000

    Pushed from Dropbox

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,43 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit1a041013a65545e6464e6c1f2aeeae46
 {
+    public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'Psr\\Log\\' => 8,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Psr\\Log\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/log/Psr/Log',
+        ),
+    );
+
+    public static $prefixesPsr0 = array (
+        'M' => 
+        array (
+            'Monolog' => 
+            array (
+                0 => __DIR__ . '/..' . '/monolog/monolog/src',
+            ),
+        ),
+        'F' => 
+        array (
+            'Fuel\\Upload' => 
+            array (
+                0 => __DIR__ . '/..' . '/fuelphp/upload/src',
+            ),
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit1a041013a65545e6464e6c1f2aeeae46::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit1a041013a65545e6464e6c1f2aeeae46::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit1a041013a65545e6464e6c1f2aeeae46::$prefixesPsr0;
 
         }, null, ClassLoader::class);
     }
