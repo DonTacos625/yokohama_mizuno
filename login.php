ab1a8e89024ece0c211ba63f297fe707d3466c69
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-11-17 06:11:39 +0000

    Pushed from Dropbox

--- login.php
+++ login.php
@@ -1,17 +1,17 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/facebook-sdk-v5/autoload.php';
 session_start();
 $fb = new Facebook\Facebook([
   'app_id' => 'ID', // Replace {app-id} with your app id
   'app_secret' => 'SECRET',
-  'default_graph_version' => 'v2.5',
+  'default_graph_version' => 'v2.7',
   ]);
 $helper = $fb->getRedirectLoginHelper();
-$permissions = ['email', 'user_likes']; // optional
-$loginUrl = $helper->getLoginUrl('https://websitetest1234.herokuapp.com/fb-callback.php', $permissions);
 
-//$loginUrl = $helper->getLoginUrl('https://websitetest1234.herokuapp.com/fb-callback.php', $permissions);
+$permissions = array(); // Optional permissions
+
+$loginUrl = $helper->getLoginUrl('https://websitetest1234.herokuapp.com/fb-callback.php', $per);
 //$loginUrl = $helper->getLoginUrl('https://websitetest1234.herokuapp.com/fb-callback.php',$permissions);
 
 echo "5";
