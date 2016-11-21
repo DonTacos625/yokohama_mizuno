28c7c1c897aed8991e362fc94c7312e8edf00bd8
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-11-21 20:40:06 +0000

    Pushed from Dropbox

--- login.php
+++ login.php
@@ -3,12 +3,12 @@
 session_start();
 
 header("Content-type: text/html; charset=utf-8");
- 
+
 require_once("config.php");
 
 $helper = $fb->getRedirectLoginHelper();
 
-$permissions = []; // Optional permissions
+$permissions = ""; // Optional permissions
 
 $loginUrl = $helper->getLoginUrl('https://websitetest1234.herokuapp.com/fb-callback.php', $permissions);
 //$loginUrl = $helper->getLoginUrl('https://websitetest1234.herokuapp.com/fb-callback.php',$permissions);
