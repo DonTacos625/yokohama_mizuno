88eb648d07c13c65962e294a53f23c0db1dc82ec
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-11-22 00:23:16 +0000

    Pushed from Dropbox

--- localinfo2.php
+++ localinfo2.php
@@ -19,7 +19,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
 		$json = file_get_contents($url);
 		$json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
 		$arr = json_decode($json,true);
-
+		var_dump($arr);
 
 		if ($arr == NULL) {
    		echo "なにもないよ！";
