a1b0a6bc8c73b9e3116f850ab0027be8e2fd4e30
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-11-22 03:46:54 +0000

    Pushed from Dropbox

--- localinfo3.php
+++ localinfo3.php
@@ -94,8 +94,35 @@ if(isset($_SESSION["my_no"])){
 							<div class="thumbnails">
 							<?php
 							if($spot_pic!=NULL){
-								//echo "<img src='".$spot_pic."' alt='".$spot_name."' width='300' height='200'>";
-								echo "<img src='".$spot_pic."' alt='Untitled'>";
+								// オリジナル画像のファイルパスを指定
+								$original_file = $spot_pic;
+
+								// getimagesize関数 オリジナル画像の横幅・高さを取得
+								list($original_width, $original_height) = getimagesize($original_file);
+
+								// サムネイルの横幅を指定
+								$thumb_width = 200;
+
+								// サムネイルの高さを算出 round関数で四捨五入
+								$thumb_height = round( $original_height * $thumb_width / $original_width );
+
+								// オリジナルファイルの画像リソース
+								$original_image = imagecreatefromjpeg($original_file);
+
+								// サムネイルの画像リソース
+								$thumb_image = imagecreatetruecolor($thumb_width, $thumb_height);
+
+								// サムネイル画像の作成
+								imagecopyresized($thumb_image, $original_image, 0, 0, 0, 0,
+								                 $thumb_width, $thumb_height,
+								                 $original_width, $original_height);
+
+								// サムネイル画像の出力
+								imagejpeg($thumb_image);
+
+								// 画像リソースを破棄
+								imagedestroy($original_image);
+								imagedestroy($thumb_image);
 							}
 							?></div>
 							</td>
