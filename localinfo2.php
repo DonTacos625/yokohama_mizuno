4f3e3a1f7009c670469cf577d286de055763f3fc
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-11-22 00:09:29 +0000

    Pushed from Dropbox

--- localinfo2.php
+++ localinfo2.php
@@ -18,6 +18,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
 		$url = "./localinfo.json";
 		$json = file_get_contents($url);
 		$json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+		echo $json;
 		$arr = json_decode($json,true);
 
 		if ($arr === NULL) {
