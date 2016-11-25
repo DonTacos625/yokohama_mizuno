f848417de6b020422aeeb71cea587fb30ea7b4bb
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-11-25 03:01:48 +0000

    Pushed from Dropbox

--- local_evaluation.php
+++ local_evaluation.php
@@ -183,6 +183,18 @@ if ($_SERVER["REQUEST_METHOD"]=="POST") {
 	<meta http-equiv="Content-type" content="text/html; charset=utf-8"></meta>
 	<title>観光スポットの評価</title>
 	<link rel="stylesheet" type="text/css" href="stylet.css"></link>
+	<!--google解析-->
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-87819413-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<!--ここまで-->
 </head>
 
 <body>
