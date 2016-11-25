ed1d683c0558ac57c957cb42888edf2dcbc8eadb
Yuri Mizutani <y.mizutani@uec.ac.jp>
2016-11-25 02:46:38 +0000

    Pushed from Dropbox

--- index.php
+++ index.php
@@ -19,6 +19,18 @@ if(isset($_SESSION["my_no"]))
 	<script type="text/javascript" src="jquery-3.1.1.min.js"></script>
 	<script type="text/javascript">
 	</script>
+<!--google解析-->
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-87819413-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<!--ここまで-->
 </head>
 <body>
 	<div id="page">
