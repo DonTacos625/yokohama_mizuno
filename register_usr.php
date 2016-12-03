23c6ca2b7d5453b737fc1d76c8ebc69cf55c31fa
ゆり <yuri@yuri-no-MacBook-Air.local>
2016-12-04 06:37:03 +0900

    make it better

--- register_usr.php
+++ register_usr.php
@@ -110,24 +110,24 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 					<table align="center" border="0" cellspacing="2" cellpadding="2"  width="600px">
 						<tr><div class="label" align="center">会員登録</div></tr>
 						<tr>
-							<td><div class="label">ユーザID</div></td>
+							<td><div class="label" align="center">ユーザID</div></td>
 							<td>
 								<input type="text" name="usr_id" value="<?=$usr_id ?>" size="30">
-								<br><font size="3">5〜30文字の半角英数字</font>
+								<br><font size="2">5〜30文字の半角英数字</font>
 							</td>
 						</tr>
 						<tr>
-							<td><div class="label">パスワード</div></td>
+							<td><div class="label" align="center">パスワード</div></td>
 							<td>
 								<input type="password" name="usr_pw" value="<?=$usr_pw ?>"><br>
-								<font size="3">6文字以上かつ半角英小文字,半角英大文字,数字を混在させたもの</font>
+								<font size="2">6文字以上かつ半角英小文字,半角英大文字,数字を混在させたもの</font>
 							</td>
 						</tr>
 						<tr>
-							<td><div class="label">確認用パスワード</div></td>
+							<td><div class="label" align="center">確認用パスワード</div></td>
 							<td>
 								<input type="password" name="usr_pw2" value="<?=$usr_pw2 ?>"><br>
-								<font size="3">もう一度パスワードの入力をお願いします</font>
+								<font size="2">もう一度パスワードの入力をお願いします</font>
 							</td>
 						</tr>
 						<tr><td align="center" colspan="2"><input type="submit" name="Submit" value="登録する"></td></tr>
