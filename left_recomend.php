957aada00a9a8376030749d6a1463a1f4a1d63bc
ゆり <yuri@yuri-no-MacBook-Air.local>
2016-12-04 05:01:26 +0900

    make it better

--- left_recomend.php
+++ left_recomend.php
@@ -1,47 +1,5 @@
 <!--左サイドメニュー-->
 <div class="subinfo">
-	<div class="label">あなたの情報</div>
-	<ul>
-		<?php
-		if(isset($_SESSION["my_no"])||isset($_SESSION["gender"])||isset($_SESSION["age"])){
-			$my_no = $_SESSION["my_no"];
-			$gender = $_SESSION["gender"];
-			$age = $_SESSION["age"];
-
-			echo "会員番号：";
-			echo (int)$my_no;
-			echo "<br>";
-			if(isset($_SESSION['fb_access_token'])){
-				echo "年　　齢：";
-				if($age == NULL)
-					echo "未記入<br>";
-				else{
-					echo $age;
-					echo "歳 <br>";
-				}
-			}else{
-				echo "年　　代：";
-				if($age == NULL)
-					echo "未記入<br>";
-				else{
-					echo $age;
-					echo "代 <br>";
-				}
-			}
-			echo "性　　別：";
-			if($gender=="male")
-				echo "男性";
-			else if($gender=="female")
-				echo "女性";
-			else if($gender==NULL)
-				echo "未記入";
-			else
-				echo $gender;
-		}else{
-			echo "ログインをお願いします";
-		}
-		?>
-	</ul>
 	<div class="label">推薦項目</div>
 	<ul>
 		<?php
@@ -59,7 +17,7 @@
 		}
 		echo "<br>";
 		echo "カテゴリー";
-		echo "<ul>"
+		echo "<ul>";
 		for($i=0;$i<6;$i++){
 			if($categorycheck[$i]!=0){
 				if($categorycheck[$i]==1){
@@ -77,8 +35,8 @@
 				}
 			}
 		}
-		echo "</ul>"
-		echo "重視する項目:"
+		echo "</ul>";
+		echo "重視する項目:";
 		if($point==0)echo "何も重視しない";
 		if($point==1)echo "満足度";
 		if($point==2)echo "アクセスのしやすさ";
