87ab3b8affb76d6939037644e4121aa0dcdf8c67
ゆり <yuri@yuri-no-MacBook-Air.local>
2016-12-04 13:11:56 +0900

    make it better

--- callback.php
+++ callback.php
@@ -63,7 +63,7 @@ if(isset( $_REQUEST['oauth_token']) && !empty( $_GET['oauth_token'] ) && isset( 
 			//Sessionの登録
 		$_SESSION["my_no"] = $no;
 		$_SESSION["anq"] = 0;
-		$first==1
+		$first==1;
 	}
 	//sessionを消す
 	unset($_SESSION['oauth_token']);
