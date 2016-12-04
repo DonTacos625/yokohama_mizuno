7eabb88e94d544a4526a55dea319e3cc9e2e7b79
ゆり <yuri@yuri-no-MacBook-Air.local>
2016-12-04 11:53:17 +0900

    make it better

--- callback.php
+++ callback.php
@@ -14,40 +14,41 @@ $request_token['oauth_token_secret'] = $_SESSION['oauth_token_secret'];
 
 //Twitterから返されたOAuthトークンと、あらかじめlogin.phpで入れておいたセッション上のものと一致するかをチェック
 if (isset($_REQUEST['oauth_token']) && $request_token['oauth_token'] !== $_REQUEST['oauth_token']) {
-    die( 'Error!' );
+	die( 'Error!' );
 }
-if(isset( $_GET['oauth_token'] ) && !empty( $_GET['oauth_token'] ) && isset( $_GET['oauth_verifier'] ) && !empty( $_GET['oauth_verifier'] ) ){
+//不正なアクセスではないかチェック
+if(isset( $_REQUEST['oauth_token']) && !empty( $_GET['oauth_token'] ) && isset( $_GET['oauth_verifier'] ) && !empty( $_GET['oauth_verifier'] ) ){
 //OAuth トークンも用いて TwitterOAuth をインスタンス化
-$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $request_token['oauth_token'], $request_token['oauth_token_secret']);
+	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $request_token['oauth_token'], $request_token['oauth_token_secret']);
 
 //アプリでは、access_token(配列になっています)をうまく使って、Twitter上のアカウントを操作していきます
-$access_token = $connection->oauth("oauth/access_token", array("oauth_verifier" => $_REQUEST['oauth_verifier']));
-/*
-ちなみに、この変数の中に、OAuthトークンとトークンシークレットが配列となって入っています。
-*/
+	$access_token = $connection->oauth("oauth/access_token", array("oauth_verifier" => $_REQUEST['oauth_verifier']));
+
+//ちなみに、この変数の中に、OAuthトークンとトークンシークレットが配列となって入っています。
+
 
 //OAuthトークンとシークレットも使って TwitterOAuth をインスタンス化
-$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
+	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
 
-//ユーザー情報をGET
-$user = $connection->get("account/verify_credentials");
-//(ここらへんは、Twitter の API ドキュメントをうまく使ってください)
+	//ユーザー情報をGET
+	$user = $connection->get("account/verify_credentials");
+	//(ここらへんは、Twitter の API ドキュメントをうまく使ってください)
 
-//GETしたユーザー情報をvar_dump
-var_dump( $user );
-echo $user["id"];
+	//GETしたユーザー情報をvar_dump
+	var_dump( $user );
+	echo $user["id"];
 
-unset($_SESSION['oauth_token']);
-unset($_SESSION['oauth_token_secret']);
-}
-elseif( isset( $_GET['denied'] ) && !empty( $_GET['denied'] ) )
-	{
+	unset($_SESSION['oauth_token']);
+	unset($_SESSION['oauth_token_secret']);
+}else if( isset( $_GET['denied'] ) && !empty( $_GET['denied'] ) ){
 		// エラーメッセージを出力して終了
-		echo 'You have rejected the app...Bye...' ;
+	echo 'You have rejected the app...Bye...' ;
 
-		// 何故か昔、迷惑ユーザーをとりあえずYahoo!に飛ばすという謎文化がありました…
-		header( 'Location: http://www.yahoo.co.jp/' ) ;
+		// ログインページに飛ばす
+	header( 'Location: https://study-yokohama-sightseeing.herokuapp.com/login.php' ) ;
 
-		exit ;
-	}
+	exit ;
+}else{
+	echo "不正なアクセス";
+}
 ?>
