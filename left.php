c22be630236a32a7fabeedb4c8b9e32678ffff68
ゆり <yuri@yuri-no-MacBook-Air.local>
2016-12-28 11:37:44 +0900

    make it better

--- left.php
+++ left.php
@@ -71,7 +71,7 @@ if ($_SERVER["REQUEST_METHOD"]=="POST"){
 		}
 		?>
 	</ul>
-	<!--<?php
+	<?php
 	if(isset($_SESSION["anq"])&&$_SESSION["anq"]==0){
 		echo "<div class='label'>アンケート</div>";
 		echo "<ul>";
@@ -81,7 +81,7 @@ if ($_SERVER["REQUEST_METHOD"]=="POST"){
 		echo "注意:<br>回答は<font color='red'><b>１度のみ</b></font>です。<br><b>推薦システムを1度以上利用してからご回答下さい</b>";
 		echo "</ul>";
 	}
-	?>^-->
+	?>
 	<div class="label">SNS</div>
 	<ul>
 		<iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fstudy-yokohama-sightseeing.herokuapp.com%2Findex.php&width=136&layout=button_count&action=like&size=small&show_faces=false&share=true&height=46&appId=783967058409220" width="136" height="46" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
